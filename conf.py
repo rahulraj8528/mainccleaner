eabc73d25ff507ce68c2dfc0de6316ddb6ed1073
rahulraj8528 <rahulraj8528@outlook.com>
2025-09-25 00:17:14 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,10 +29,12 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
+html_title = "Enter and Activate Your CCleaner Professional License Key After Download and Installation"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
 
 # If you want to add other meta tags, do so in your _templates/layout.html
+
 
 
